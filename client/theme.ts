833a6bd201bc5a26b83e5400b1b2e064896610d0
AL2002MI08 <alexandramizero@gmail.com>
2025-08-08 20:27:28 +0200

    chore: initial setup

---
+++ client/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9333ea",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
