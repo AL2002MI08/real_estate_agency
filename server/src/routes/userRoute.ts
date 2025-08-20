9250301aeefe91f2d8d653b41819a152a0f22c16
AL2002MI08 <alexandramizero@gmail.com>
2025-08-20 17:50:19 +0200

    chore: update user routes

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -16,10 +16,9 @@ const router = express.Router();
 
 router.post("/register",validate(registerSchema),  register);
 router.post("/login", validate(loginSchema), login);
-router.get("/profile", getUserProfile);
-router.post("/bookVisit/:id", jwtCheck, bookVisit);
 router.post("/allBookings", getAllBookings);
+router.get("/allFav",jwtCheck, getAllFavorites);
+router.post("/bookVisit/:id", jwtCheck, bookVisit);
 router.post("/removeBooking/:id", jwtCheck, cancelBooking);
 router.post("/toFav/:rid", jwtCheck, toFav);
-router.post("/allFav/", jwtCheck, getAllFavorites);
 export { router as userRoute };
