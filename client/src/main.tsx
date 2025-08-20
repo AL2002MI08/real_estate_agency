a0d623ac698e5a21a53a104bd3ee4c1fb0058424
AL2002MI08 <alexandramizero@gmail.com>
2025-08-20 07:17:33 +0200

    chore: wrap main app with auth provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from '../theme.ts'
+import { AuthProvider } from './contexts/authContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <ThemeProvider theme={theme}>
-  <CssBaseline />
-  <App />
-</ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 )
